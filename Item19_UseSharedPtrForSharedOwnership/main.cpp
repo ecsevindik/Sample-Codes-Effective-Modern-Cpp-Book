c3274f4648e840bb058b5482c2d84079fd95358b
Rulokat <mrcnsvndk@gmail.com>
2022-06-02 22:01:39 +0300

    Improve item 19

--- Item19_UseSharedPtrForSharedOwnership/main.cpp
+++ Item19_UseSharedPtrForSharedOwnership/main.cpp
@@ -96,8 +96,8 @@ void test2() {
         delete inv;
     };
 
-    std::shared_ptr<Investment> pw1 = std::shared_ptr<Investment>(new Bond(10), customDel1);
-    std::shared_ptr<Investment> pw2 = std::shared_ptr<Investment>(new Stock(20), customDel2);
+    auto pw1 = std::shared_ptr<Investment>(new Bond(10), customDel1);
+    auto pw2 = std::shared_ptr<Investment>(new Stock(20), customDel2);
 
     std::vector<std::shared_ptr<Investment>> vpw;
     vpw.emplace_back(std::move(pw1));
@@ -112,3 +112,64 @@ int main() {
     
     return 0;
 }
+
+/* std::enable_shared_from_this<T> explanation
+
+std::vector<std::shared_ptr<Widget>> processedWidgets;
+
+class Widget: public std::enable_shared_from_this<Widget> {
+public:
+    …
+    void process();
+    …
+};
+
+void Widget::process() // process the Widget add it to list of processed Widgets; this is wrong!
+{
+    …
+    processedWidgets.emplace_back(this);
+}
+
+
+The std::shared_ptr thus constructed will create a new control block for the pointed-to Widget (*this). 
+That doesn’t sound harmful until you realize that if there are std::shared_ptrs outside
+the member function that already point to that Widget, it’s game, set, and match for
+undefined behavior.
+
+The std::shared_ptr API includes a facility for just this kind of situation. It has
+probably the oddest of all names in the Standard C++ Library: std::
+enable_shared_from_this. That’s a template for a base class you inherit from if
+you want a class managed by std::shared_ptrs to be able to safely create a
+std::shared_ptr from a this pointer. In our example, Widget would inherit from
+std::enable_shared_from_this as follows:
+
+class Widget: public std::enable_shared_from_this<Widget> {
+public:
+    …
+    void process();
+    …
+};
+
+std::enable_shared_from_this defines a member function that creates a
+std::shared_ptr to the current object, but it does it without duplicating control
+blocks. The member function is shared_from_this, and you use it in member func‐
+tions whenever you want a std::shared_ptr that points to the same object as the
+this pointer. Here’s a safe implementation of Widget::process:
+
+void Widget::process()
+{
+    // as before, process the Widget
+    …
+    // add std::shared_ptr to current object to processedWidgets
+    processedWidgets.emplace_back(shared_from_this());
+}
+
+Internally, shared_from_this looks up the control block for the current object, and
+it creates a new std::shared_ptr that refers to that control block. The design relies
+on the current object having an associated control block. For that to be the case, there
+must be an existing std::shared_ptr (e.g., one outside the member function calling
+shared_from_this) that points to the current object. If no such std::shared_ptr
+exists (i.e., if the current object has no associated control block), behavior is unde‐
+fined, although shared_from_this typically throws an exception.
+
+*/
