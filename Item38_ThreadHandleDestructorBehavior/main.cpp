0c1c8a81a2388a6af8786dd970133dd5baa0bb90
Rulokat <mrcnsvndk@gmail.com>
2022-06-03 18:25:56 +0300

    Add packagedTast test to item 38

--- Item38_ThreadHandleDestructorBehavior/main.cpp
+++ Item38_ThreadHandleDestructorBehavior/main.cpp
@@ -1,5 +1,5 @@
 
-/**
+/*
 • Future destructors normally just destroy the future’s data members.
 • The final future referring to a shared state for a non-deferred task launched
 via std::async blocks until the task completes.
@@ -12,33 +12,63 @@ via std::async blocks until the task completes.
 using namespace std::literals;
 
 int doAsyncWork(int sleepms) {
-    std::cout << "Async work performed - " << sleepms << std::endl;
+    std::cout << "Async work performing - " << sleepms << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds{sleepms});
     return 10;
 }
 
 int doDeferredWork(int sleepms) {
-    std::cout << "Deferred work performed - " << sleepms << std::endl;
+    std::cout << "Deferred work performing - " << sleepms << std::endl;
     std::this_thread::sleep_for(std::chrono::milliseconds{sleepms});
     return 5;
 }
 
-void test1() {
+void sharedStateTest() {
+    std::cout << "sharedStateTest" << std::endl;
+
     std::shared_future<int> shared1 = std::async(std::launch::async, doAsyncWork, 10);
     {
         auto shared2 = shared1;
-    }
+    } // shared2 is destructed here, however, async work does not block the main thread since there is another shared state pointing to async's promise.
     std::cout << "First scope exited" << std::endl;
 
     {
         std::future<int> fut1 = std::async(std::launch::async, doAsyncWork, 20);
-    }
+    }// since fut1 is destructed here, code is blocked to wait for async work to complete since there is no other future waiting for async's work
     std::cout << "Second scope exited" << std::endl;
 
+    std::cout << std::endl;
+
+}
+
+// Dummy function
+int calcValue () { return 3; }
+
+/*
+The class template std::packaged_task wraps any Callable target 
+(function, lambda expression, bind expression, or another function object) 
+so that it can be invoked asynchronously. Its return value or exception thrown 
+is stored in a shared state which can be accessed through std::future objects. 
+*/
+void packagedTaskTest() {
+    std::cout << "packagedTaskTest" << std::endl;
+
+    std::packaged_task<int()> pt(calcValue);
+
+    auto fut = pt.get_future();
+
+    std::thread t(std::move(pt));
+
+    t.join(); // Blocks this thread until t finishes. Otherwise, t becomes joinable at the end of the scope and program is terminated.
+
+    std::cout << fut.get() << std::endl;
+
+    std::cout << std::endl;
 }
 
 int main() {
     
-    test1();
+    sharedStateTest();
+    packagedTaskTest();
     return 0;
 }
