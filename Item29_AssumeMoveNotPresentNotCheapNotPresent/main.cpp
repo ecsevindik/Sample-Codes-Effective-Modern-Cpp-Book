5a4fb7a1e22fc00b7ab0c1ce951fd3cc06ed24de
Rulokat <mrcnsvndk@gmail.com>
2022-06-03 13:13:47 +0300

    Improve comments in item 29

--- Item29_AssumeMoveNotPresentNotCheapNotPresent/main.cpp
+++ Item29_AssumeMoveNotPresentNotCheapNotPresent/main.cpp
@@ -12,6 +12,22 @@ assumptions.
 using namespace utils;
 
 /*
+There are thus several scenarios in which C++11’s move semantics do you no good:
+  • No move operations: The object to be moved from fails to offer move opera‐
+  tions. The move request therefore becomes a copy request.
+  • Move not faster: The object to be moved from has move operations that are no
+  faster than its copy operations.
+  • Move not usable: The context in which the moving would take place requires a
+  move operation that emits no exceptions, but that operation isn’t declared noex
+  cept.
+  
+It’s worth mentioning, too, another scenario where move semantics offers no effi‐
+ciency gain:
+  • Source object is lvalue: With very few exceptions (see e.g., Item 25) only rvalues
+  may be used as the source of a move operation.
+*/
+
+/*
 
 * Widget class has no move constructor, therefore copy ctor is used when it is moved.
 * WidgetNoExcept class has noexcept move ctor, which makes moving it much faster
